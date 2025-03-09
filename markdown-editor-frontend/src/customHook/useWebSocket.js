c54726cc653dca8325ed24bccdef140f47841adb
syedshahid19 <syedshahid9550@gmail.com>
2025-03-09 07:25:51 +0530

    Added backend link in frontend

--- markdown-editor-frontend/src/customHook/useWebSocket.js
+++ markdown-editor-frontend/src/customHook/useWebSocket.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://markdown-editor-app.onrender.com");
 
 export default function useWebSocket(setHtml) {
   useEffect(() => {
